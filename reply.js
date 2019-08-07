4a352a585743da234d55198994d87bfe89e913b0
obnoxiouusnerd <pdkarawale31@gmail.com>
2019-08-07 20:07:57 +0530

    changed voice to uk english female

--- reply.js
+++ reply.js
@@ -1,17 +1,18 @@
 //arrays containing *very good* replies
-const greeting = ["i am sick of you", "do you really care", "i am dying", "i am good you little dumb fellow"];
+const greeting = ["i am sick of you", "i know you want to flatter me now", "i am dying", "i am good you little dumb fellow"];
 const weather = ["weather is fine, checked on accuweather.com", "just go out and check","i don't know dude"];
 function reply(transcript) {
-    responsiveVoice.setDefaultVoice("US English Male");
+    responsiveVoice.setDefaultVoice("UK English Female");
      if (transcript.includes(("how"||"what"||"today")&&"weather")){
         const finalText  = weather[Math.floor(Math.random()*weather.length)];
          responsiveVoice.speak(finalText);
      }
-     else if (transcript.includes("how are you"||"how is it going"||"are you fine")) {
+     else if (transcript.includes('how are you') || transcript.includes('are you fine') || transcript.includes('how are you doing today')) {
         const finalText = greeting[Math.floor(Math.random()*greeting.length)];
          responsiveVoice.speak(finalText);
      }
+
      else{
-         responsiveVoice.speak("Couldn't get that. Please seek my dev for this");
+         responsiveVoice.speak("Couldn't get that.");
      }
  }
