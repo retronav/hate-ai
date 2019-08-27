678c0b962d29f6f73f1027461424ec0b1c005bdf
obnoxiousnerd <pdkarawale31@gmail.com>
2019-08-27 23:04:11 +0530

    addition of day, date, time

---
+++ inputs.js
@@ -0,0 +1,16 @@
+var now = new Date();
+
+
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+const greeting = ["I am sick of you.", "I know you want to flatter me now.", "I am dying.", "I am good you little dumb fellow."];
+const weather = ["Weather is fine, checked on accuweather.com.", "Just go out and check.","I don't know."];
+
+
+
+const whoQ = ["who are you"];
+const timeQ = ["what is the time", "current time", "tell me the time", "time", "time please"];
+const dateQ  = ["what is the date", "date", "current date", "tell me the date", "tell me the date please", "what is today's date"];
+const dayQ =[ "tell me what is this day","what is today's day", "weekday", "which day is this", "tell me today's weekday", "tell me today's day"];
--- reply.js
+++ reply.js
@@ -1,18 +1,46 @@
 //arrays containing *very good* replies
-const greeting = ["i am sick of you", "i know you want to flatter me now", "i am dying", "i am good you little dumb fellow"];
-const weather = ["weather is fine, checked on accuweather.com", "just go out and check","i don't know dude"];
+
 function reply(transcript) {
     responsiveVoice.setDefaultVoice("UK English Female");
      if (transcript.includes(("how"||"what"||"today")&&"weather")){
-        const finalText  = weather[Math.floor(Math.random()*weather.length)];
+        var finalText  = weather[Math.floor(Math.random()*weather.length)];
          responsiveVoice.speak(finalText);
-     }
+         document.getElementById("replx").innerHTML= finalText;
+        }
      else if (transcript.includes('how are you') || transcript.includes('are you fine') || transcript.includes('how are you doing today')) {
-        const finalText = greeting[Math.floor(Math.random()*greeting.length)];
+        var finalText = greeting[Math.floor(Math.random()*greeting.length)];
          responsiveVoice.speak(finalText);
-     }
+         document.getElementById("replx").innerHTML= finalText;
+        }
+     else if(timeQ.includes(transcript)){
+        var time = now.getHours() + " hours " + now.getMinutes() + " minutes. ";
+        var finalText = "The time is "+time;
+        responsiveVoice.speak(finalText);
+        document.getElementById("replx").innerHTML= finalText;
+    }
+    else if(dateQ.includes(transcript)){
+        var date = now.getDate() + " " + month[now.getMonth()] + " " + now.getFullYear();
+        var finalText = "The date is "+date;
+        responsiveVoice.speak(finalText);
+        document.getElementById("replx").innerHTML= finalText;
+
+    }
+    else if(dayQ.includes(transcript)){
+        var day = weekday[now.getDay()];
+        var finalText = "Today is "+day;
+        responsiveVoice.speak(finalText);
+        document.getElementById("replx").innerHTML= finalText;
+    }
+    else if(whoQ.includes(transcript)){
+        var finalText = "I am a personal assistant that is useful but unnamed because my dev went mad making me.";
+        responsiveVoice.speak(finalText);
+        document.getElementById("replx").innerHTML= finalText;
+    }
+
 
      else{
-         responsiveVoice.speak("Couldn't get that.");
+         var finalText = "Couldn't get that."
+         responsiveVoice.speak(finalText);
+         document.getElementById("replx").innerHTML= finalText;
      }
  }
