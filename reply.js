f3138dd94edd81133615509642ed9d7507770343
obnoxiousnerd <pdkarawale31@gmail.com>
2019-09-01 12:14:46 +0530

    finally, weather added!

--- reply.js
+++ reply.js
@@ -36,7 +36,32 @@ function reply(transcript) {
         responsiveVoice.speak(finalText);
         document.getElementById("replx").innerHTML= finalText;
     }
-
+    else if(transcript.startsWith('show weather')){
+        var city = transcript.split("weather").pop();
+        let searchLink = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+appkey;
+        function theResponse(response) {
+            let jsonObject = JSON.parse(response);
+            var name = jsonObject.name;
+            var temp = parseInt(jsonObject.main.temp - 273) + "° C";
+            var desc = jsonObject.weather[0].description;
+            var humidity= jsonObject.main.humidity + "%";
+            document.getElementById("replx").innerHTML = name + " has temperature of "+ temp + " and humidity is " +humidity+ ". It is currently experiencing "+desc+".";
+            var finalText = document.getElementById("replx").textContent;
+            responsiveVoice.speak(finalText);
+        }
+    function httpRequestAsync(url, callback)
+ {
+   console.log("loaded");
+     var httpRequest = new XMLHttpRequest();
+     httpRequest.onreadystatechange = () => { 
+         if (httpRequest.readyState == 4 && httpRequest.status == 200)
+             callback(httpRequest.responseText);
+     }
+     httpRequest.open("GET", url, true); // true for asynchronous 
+     httpRequest.send();
+ }
+        httpRequestAsync(searchLink, theResponse);
+    }
 
      else{
          var finalText = "Couldn't get that."
